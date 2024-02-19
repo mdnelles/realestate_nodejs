5838a0adb8a62f1af65b13d727e54633c2fe6ab5
Michael Nelles <mxnelles@gmail.com>
2024-02-19 08:54:34 -0500

    test hook

--- src/routes/TestRoute.ts
+++ src/routes/TestRoute.ts
@@ -7,3 +7,11 @@ export const test = async (req: Req, res: Res): Promise<any> => {
     res.json({ status: 201, err: true, msg: 'fail1', error });
   }
 };
+
+export const test2 = async (req: Req, res: Res): Promise<any> => {
+  try {
+    res.json({ status: 200, err: false, msg: 'ok 2' });
+  } catch (error) {
+    res.json({ status: 201, err: true, msg: 'fail1', error });
+  }
+};
