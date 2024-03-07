c6e8aac88d8bdc9ba0fba642d313fe4921d7c752
Michael Nelles <mxnelles@gmail.com>
2024-03-07 16:02:04 -0500

    image association

---
+++ src/database/models/images.ts
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+import { db } from '../db';
+
+export const Images: any = db.sequelize.define(
+  'images',
+  {
+    imageName: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+    isDeleted: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
